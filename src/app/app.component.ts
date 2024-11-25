3140490c13f5c51306e14a5c38153a37e9d30aac
Jham Giron <jhampiergiron@gmail.com}>
2024-11-25 01:35:33 -0500

    feat: finish added photos and generate the title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import OpenAI from "openai";
+import { ArrayImages } from '../data/ImagesData';
+import { HttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
@@ -14,11 +17,32 @@ export class AppComponent implements OnInit {
     apiKey: 'API_KEY',
     dangerouslyAllowBrowser: true
   });
-  prompt = 'Genera un título en inglés describiendo lo que hay en la imagen adjunta, con una extensión máxima de 200 caracteres para Shutterstock y Adobe Stock. La ubicación del lugar es: Pueblito boyacense, Duitama, Boyaca. Proporciona exactamente 50 palabras clave en inglés en un solo párrafo, separadas por comas, sin tildes y todas en minúsculas. Usa las siguientes palabras clave y asegúrate de que cada una esté separada por comas, incluso las que tu agregues: historical, colonial, colonial house, colombian architecture, vacation, landmark, ancient, colombian, colombian art, decorated, tourist attraction, touristic, boyacá, colombia, boyaca colombia, duitama boyaca, duitama, boyaca town, little town boyacense, boyacense town, pueblo boyaca, pueblito boyacense, pueblo boyacense, historic, traditional, culture, architecture, travel. Busca imágenes relacionadas a la imagen en la web para mejorar las palabras clave para Shutterstock y Adobe Stock. Usa palabras sencillas y fáciles de leer. Asegúrate de que todas las palabras clave estén separadas por comas y si es necesario agrega palabras para completar las 50 palabras claves. En el titulo usa Litle town Boyacense en lugar de Pueblito Boyacense. Tómate tu tiempo en hacerlo y asegúrate de que la respuesta sea precisa y exacta';
+  prompt = 'Genera un título en inglés describiendo lo que hay en la imagen adjunta, con una extensión máxima de 200 caracteres para Shutterstock y Adobe Stock. La ubicación del lugar es: Bogotá, Colombia. Proporciona exactamente 50 palabras clave en inglés en un solo párrafo, separadas por comas, sin tildes y todas en minúsculas. Usa las siguientes palabras clave y asegúrate de que cada una esté separada por comas, incluso las que tu agregues: modern architecture, bogota city, bogota colombia, bogota, colombia, colombian culture, iconic buildings, city squares, street photography, commercial, building, district capital, bogota architecture, aerial landscape, landmark, capital, south america, modern, tourism, business, outdoors, architecture, urban, travel, landscape, city center, cityscape, buildings, travel. Busca imágenes relacionadas a la imagen en la web para mejorar las palabras clave para Shutterstock y Adobe Stock. Usa palabras sencillas y fáciles de leer. Asegúrate de que todas las palabras clave estén separadas por comas y si es necesario agrega palabras para completar las 50 palabras claves. Tómate tu tiempo en hacerlo y asegúrate de que la respuesta sea precisa y exacta';
   promptEditorial = '';
-  
+  listImages: any[] = [];
+
+  constructor(private http: HttpClient) { }
+
   ngOnInit(): void {
     this.analyzeImages();
+    this.getImages();
+  }
+
+  getImages() {
+    this.http.get('http://localhost:3000/photos').subscribe((resp: any) => {
+      this.listImages = resp;
+      console.log(this.listImages);
+    })
+  }
+
+  saveImage(body: any) {
+    this.http.post('http://localhost:3000/photos', { ...body }, {
+      headers: {
+        'Accept': 'application/json'
+      }
+    }).subscribe((resp: any) => {
+      console.log(resp);
+    });
   }
 
   async convertImageToBase64(imagePath: string): Promise<string> {
@@ -32,34 +56,62 @@ export class AppComponent implements OnInit {
     });
   }
 
-  async analyzeImages() {
-    // for(let image of ArrayImages){      
-    //   const imagePath = `images/${image}`;
-    //   try {
-    //     const base64Image = await this.convertImageToBase64(imagePath);
-    //     const completion:any = await this.openai.chat.completions.create({
-    //     model: 'gpt-4o-mini',
-    //     messages: [
-    //       {
-    //         "role": "user",
-    //         "content": [
-    //             {"type": "text", "text": "dsfdsf"},
-    //             {
-    //               "type": "image_url",
-    //               "image_url": { url: base64Image }
-    //             },
-    //           ],
-    //         }
-    //       ],
-    //     });
-    //   // console.log(completion.choices[0].message.content);
-    //   } catch (error) {
-    //     console.error('Error analizando la imagen:', error);
-    //   }
-    // }
+  async saveImageDescription(image: any, imagePath: any) {
+    const base64Image = await this.convertImageToBase64(imagePath);
+    const completion: any = await this.openai.chat.completions.create({
+      model: 'gpt-4o-mini',
+      messages: [
+        {
+          "role": "user",
+          "content": [
+            { "type": "text", "text": this.prompt },
+            {
+              "type": "image_url",
+              "image_url": { url: base64Image }
+            },
+          ],
+        }
+      ],
+    });
+
+    const dataResponse = completion.choices[0].message.content;
+    // Extraer el título
+    const titleMatch = dataResponse.match(/(?<=\*\*Title:\*\*).+/);
+    const title = titleMatch ? titleMatch[0].trim() : "";
+
+    // Extraer las keywords
+    const keywordsMatch = dataResponse.match(/(?<=\*\*Keywords:\*\*).+/);
+    const keywords = keywordsMatch ? keywordsMatch[0].trim() : "";
+    this.saveImage({ name: image, description: title, keywords });
+    console.log(title);
   }
 
-  copyValue(){
-    navigator.clipboard.writeText('foto de ballena con atardecer');
+  async analyzeImages() {
+    for (let image of ArrayImages) {
+      const imagePath = `images/${image}`;
+      try {
+        // verificar imagen ya consultada
+        this.http.get(`http://localhost:3000/photos/search?name=${image}`).subscribe((resp: any) => {
+          if (resp.length === 0) {
+            this.saveImageDescription(image, imagePath);
+          }
+        });
+        console.log(image);
+      } catch (error) {
+        console.error('Error analizando la imagen:', error);
+      }
+    }
+  }
+
+  copyValue(value:string) {
+    value = value.replace(/\.$/, '');
+    navigator.clipboard.writeText(value);
+  }
+
+  soldPhoto(id: string) {
+    this.http.put(`http://localhost:3000/photos/sold/${id}`, null).subscribe((resp:any) =>{
+      console.log(resp);
+      this.getImages();
+    })
   }
 }
